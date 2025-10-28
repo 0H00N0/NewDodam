8e86188ff7a9687977bd18cef6f3263b6532e432
chan <chanbul7676@gmail.com>
2025-10-28 13:39:55 +0900

    3차 커밋

--- Dodam/src/main/java/com/dodam/config/WebConfig.java
+++ Dodam/src/main/java/com/dodam/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
      @Override
      public void addCorsMappings(CorsRegistry registry) {
          registry.addMapping("/**")
-                 .allowedOrigins("http://localhost:3000")
+                 .allowedOrigins("http://localhost:3000", "http://3.36.41.252:3000")
                  .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                  .allowedHeaders("*")
                  .allowCredentials(true);
